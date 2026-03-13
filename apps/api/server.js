3a82a8d333d92e11a8ef60375d00483e20b45b23
Aryn-Lokare <aryanlokare077@gmail.com>
2026-03-13 14:20:00 +0530

    created monorepo structure for mobile and webapp

---
+++ apps/api/server.js
@@ -0,0 +1,29 @@
+import express from "express"
+import prisma from "./src/lib/prisma.js"
+
+const app = express()
+app.use(express.json())
+
+app.post("/users", async (req, res) => {
+  const { email, name, role } = req.body
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      role
+    }
+  })
+
+  res.json(user)
+})
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+app.listen(5000, () => {
+  console.log("Server running")
+})
+
